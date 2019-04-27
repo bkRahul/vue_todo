ddd6c38fdf4cdb452973319dc00986a4ad222162
bkRahul <rahulsatya1068@gmail.com>
2019-04-27 20:11:27 +0530

    added header component

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="header">
+
+        <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">Todo List</h1>
+            </div>
+        </div>
+        </section>
+
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
