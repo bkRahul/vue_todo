80fe2a1e14487ba8f413e317d5227b6af9d06dfd
bkRahul <rahulsatya1068@gmail.com>
2019-04-27 20:12:24 +0530

    added todos component

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="hello">
-
-    <h1>Todos</h1>
+  <div class="container box">
     <div v-bind:key="todo.id" v-for="todo in todos">
-      <TodoItem v-bind:todo="todo" />
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
     </div>
   </div>
 </template>
@@ -19,23 +17,3 @@ export default {
   props: ["todos"]
 }
 </script>
-
-
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
